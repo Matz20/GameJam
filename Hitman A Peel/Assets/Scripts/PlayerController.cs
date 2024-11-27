78db3dd3a3bd8f019030a147da6febca1258df4c
dudeonmars <nande24@student.aau.dk>
2024-11-27 09:58:41 +0100

    Squashed commit of the following:
    commit 94beba500e64947386f64b621db553edc0615a5e
    Author: dudeonmars <nande24@student.aau.dk>
    Date:   Wed Nov 27 09:11:39 2024 +0100
    
        Basic player input + extras commit
    
    commit 41d39e3499ccbd48594502dab0b9ba317a586878
    Author: dudeonmars <nande24@student.aau.dk>
    Date:   Tue Nov 26 11:14:04 2024 +0100
    
        Nikolaj setup commit

--- Hitman A Peel/Assets/Scripts/PlayerController.cs
+++ Hitman A Peel/Assets/Scripts/PlayerController.cs
@@ -1,39 +1,138 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class Playercontroller : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+    // Unity menu for setting the walk speed   
+    [Header("Movement Speeds")]
+    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private float projectileSpeed = 10.0f;
+ 
+    // Variable for the character controller
+    private CharacterController characterController;
+    // Variable for the player input handler
+    private PlayerInputHandler inputHandler;
+    // Variable for the current movement
+    private Vector2 currentMovement;
+    // Variable for the weapon to pick up
+    private GameObject weaponToPickUp;
+    // Variable for the weapon equipped
+    private GameObject weaponEquipped;
+    // Variable for the camera transform
+    private Transform cameraTransform;
+    // Variable for the look direction
+    private Vector3 lookDirection;
 
-    public float moveSpeed = 5f;
-    public Rigidbody2D rb; 
-    public weapon weapon;
-
-    Vector2 moveDirection;
-    Vector2 mousePosition;
-
-    // Update is called once per frame
-    void Update()
-    {
-        float moveX = Input.GetAxisRaw("Horizontal"); 
-        float moveY = Input.GetAxisRaw("Vertical");
-    if(Input.GetMouseButtonDown(0))
-    {
-        weapon.Fire();
+    // Awake method to get the character controller and input handler and camera transform
+    private void Awake() {
+        characterController = GetComponent<CharacterController>();
+        inputHandler = PlayerInputHandler.Instance;
+        cameraTransform = Camera.main.transform.parent;
     }
-        moveDirection = new Vector2(moveX, moveY).normalized;
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+    // Update method to handle movement, attacking, picking up, and scrolling
+    private void Update() {
+        HandleMovement();
+        HandleAttacking();
+        HandlePickUp();
+        HandleScrolling();
+        HandleLooking();
+        UpdateCameraPosition();
     }
 
-    void FixedUpdate()
-    {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    // Method to handle movement
+    void HandleMovement() {
+        float speed = walkSpeed;
+
+        // moveDirection is the input from the player
+        Vector2 moveDirection = new Vector2(inputHandler.MoveInput.x, inputHandler.MoveInput.y);
+        currentMovement = moveDirection.normalized * speed;
+
+        characterController.Move(currentMovement * Time.deltaTime);
+    }
+
+    // Method to handle attacking
+    void HandleAttacking() {
+        if(inputHandler.AttackTriggered) {
+            Debug.Log("Attacking");
+        }
+
+        // If the attack button is pressed and the player has a weapon equipped, attack in the direction the player is looking
+        if(inputHandler.AttackTriggered && weaponEquipped != null) {
+            Debug.Log("Player is Attacking");
+            GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            Rigidbody2D projectileRigidbody = projectile.GetComponent<Rigidbody2D>();
+            projectileRigidbody.velocity = lookDirection.normalized * projectileSpeed;
+        }
+    }
+
+    // Method to handle picking up things
+    void HandlePickUp() {
+        // If the pick up button is pressed, log that the player is picking up
+        if(inputHandler.PickUpTriggered) {
+            Debug.Log("Picking Up");
+        }
+
+        // If the player is near a weapon and the pick up button is pressed, pick up the weapon and set it as the weapon equipped and child to the player
+        if(inputHandler.PickUpTriggered && weaponToPickUp != null) {
+            Debug.Log("Picking Up Weapon");
+            weaponToPickUp.transform.SetParent(transform);
+            weaponToPickUp.transform.localPosition = new Vector2(1, 0);
+            Collider weaponCollider = weaponToPickUp.GetComponent<Collider>();
+            if(weaponCollider != null) {
+                weaponCollider.enabled = false;
+            }
+            weaponEquipped = weaponToPickUp;
+            weaponToPickUp = null;
+        }
+
         
-        Vector2 aimDirection = mousePosition - rb.position;
-        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = aimAngle;
     }
+
+    // Method to handle scrolling
+    void HandleScrolling() {
+        // If the scroll value is not 0, log that the player is scrolling up or down
+        if(inputHandler.ScrollValue != 0) {
+            if (inputHandler.ScrollValue > 0) {
+                Debug.Log("Scrolling Down");
+            } else {
+                Debug.Log("Scrolling Up");
+            }
+        }
+    }
+
+    // Method to handle looking at the mouse position
+    void HandleLooking() {
+        Debug.Log($"Looking at {inputHandler.LookInput}");
+        Vector2 mousePosition = inputHandler.LookInput;
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        lookDirection = mouseWorldPosition - transform.position;
+        lookDirection.z = 0;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    // Method to update the camera position to follow the player
+    void UpdateCameraPosition() {
+        if (cameraTransform != null) {
+            cameraTransform.position = new Vector3(transform.position.x, transform.position.y, cameraTransform.position.z);
+        }
+    }
+
+    // OnTriggerEnter that are weapons and colliding with player to pick up 
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Weapon")) {
+            weaponToPickUp = other.gameObject;
+        }
+    }
+    // OnTriggerExit that are weapons and not colliding with player reset weaponToPickUp
+    private void OnTriggerExit(Collider other) {
+        if(other.CompareTag("Weapon")) {
+            weaponToPickUp = null;
+        }
+    }
+
 }
-
-
