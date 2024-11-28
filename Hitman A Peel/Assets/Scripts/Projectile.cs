9abf0ab1e3fc7384f5143fa1e5a93954b75cd444
MasterIwan <114767161+MasterIwan@users.noreply.github.com>
2024-11-28 17:30:04 +0100

    changes

--- Hitman A Peel/Assets/Scripts/Projectile.cs
+++ Hitman A Peel/Assets/Scripts/Projectile.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
+
+    [SerializeField] float DMG;
+
+    void Start() { 
+    
+       
+    }
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.collider.CompareTag("Enemy")) {
             Debug.Log("Projectile hit enemy");
